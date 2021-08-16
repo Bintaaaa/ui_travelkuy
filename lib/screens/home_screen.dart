5865b38f7fb61c6ac20d62c034b1156b7c17e1ec
Bintaaaa <bijantium.yt.32@gmail.com>
2021-08-17 01:35:05 +0700

    bottom bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:basic_implement_ui_3/constans/color_constans.dart';
+import 'package:basic_implement_ui_3/widgets/bottom_navigation_travelkuy.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -11,10 +12,14 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //setting Appbar
         appBar: AppBar(
           backgroundColor: mBackgroundColor,
           title: SvgPicture.asset('assets/svg/travelkuy_logo.svg'),
+          elevation: 0 ,//for delete shadow
         ),
+      backgroundColor: mBackgroundColor,
+      bottomNavigationBar: BottomNavigationTravelkuy(),
     );
   }
 }
